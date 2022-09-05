373afa5964d34c0af540508aec9cc2c18b746f60
Sebastian Veum <sebastianveum@Sebastians-MacBook-Air.local>
2022-09-05 15:27:12 +0200

    first commit

--- main.py
+++ main.py
@@ -1,26 +1,17 @@
-import pandas as pd
 import requests
-
-class Main:
-    def __init__(self):
-        global ticker 
-        global apiKey
-        ticker=input("Select ticker: ")
-        apiKey="71d797ce96bd5189d62ea531ef4f847d"
-
-    def getCFS():    
-        global apiKey
-        get_cfs=requests.get(f"fhttps://fmpcloud.io/api/v3/cash-flow-statement/AAPL?limit=120&apikey={apiKey}").json()
-        print(get_cfs)
-
-    
-    def calcCFS():
-        return None
-o=Main()
-o.getCFS
+import matplotlib.pyplot as plt
 
 
-        
+api_key=open('apiKey.txt', 'r').read()
+api_key=str(api_key)
+ticker="AAPL"
+yr=10
+cf_statement=requests.get(f'https://financialmodelingprep.com/api/v3/cash-flow-statement/{ticker}?limit={yr}&apikey={api_key}')
 
+cf_statement=cf_statement.json()
 
+cashflow=list(reversed([cf_statement[i]['operatingCashFlow'] for i in range(len(cf_statement))]))
+
+plt.plot(cashflow, label="Cashflow")
+plt.show()
 
